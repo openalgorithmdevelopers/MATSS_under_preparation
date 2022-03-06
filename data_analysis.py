c225b0a67ffd8ab8638abd42fe804f4a7d80e9c5
Bhupendra Singh <bhupendrabisht59@gmail.com>
2022-03-06 10:28:59 +0530

    working on t test

--- data_analysis.py
+++ data_analysis.py
@@ -35,73 +35,68 @@ def findOutliers(df, column_name):
     # print(new_df.shape)
     return new_df
 
-totalSubjects = 21
-totalUtterances = 60    #the number of utterances of words in a folder for every subject
-featureName = "mfcc"
-FEATURE_SIZE = 128
+def plotHistogram(data):
+    fig, axs = plt.subplots(1, 1,
+                            figsize =(10, 7),
+                            tight_layout = True)
+     
+    axs.hist(data, bins = 40)
+     
+    # Show plot
+    plt.show()
 
-dataset_all = pd.read_csv ('master_dataset.csv')
-Y = dataset_all.iloc[:,3]
-X = dataset_all.iloc[:,4:]
 
-X_0 = dataset_all[dataset_all["TrueClass"] == 0]
+## #sns.boxplot(X_0.flatten())
+#sns.displot(X_0_mean_feature)
+#plt.show()
+
+
+def perform_t_test(group1, group2):
+    t_value,p_value=stats.ttest_rel(group1,group2)
+    
+    print('Test statistic is %f'%float("{:.6f}".format(t_value)))
+    
+    print('p-value for two tailed test is %f'%p_value)
+
+dataset = pd.read_csv ('master_dataset.csv')
+Y = dataset.iloc[:,3]
+X = dataset.iloc[:,4:]
+
+X_0 = dataset[dataset["TrueClass"] == 0]
 X_0 = X_0.iloc[:,4:]
-X_1 = dataset_all[dataset_all["TrueClass"] == 1]
+X_1 = dataset[dataset["TrueClass"] == 1]
 X_1 = X_1.iloc[:,4:]
-X_2 = dataset_all[dataset_all["TrueClass"] == 2]
+X_2 = dataset[dataset["TrueClass"] == 2]
 X_2 = X_2.iloc[:,4:]
 
 
+def rescale(data, mi, mx):
+    minmax_scale = preprocessing.MinMaxScaler(feature_range=(mi, mx))
+    return(minmax_scale.fit_transform(data))
 
-minmax_scale = preprocessing.MinMaxScaler(feature_range=(0, 1))
+X_0 = rescale(X_0, 0, 1)
+X_1 = rescale(X_1, 0, 1)
+X_2 = rescale(X_2, 0, 1)
 
-X_0 = minmax_scale.fit_transform(X_0)
-X_1 = minmax_scale.fit_transform(X_1)
-X_2 = minmax_scale.fit_transform(X_2)
+X_0_mean_feature = np.mean(X_0, axis=1)
+X_1_mean_feature = np.mean(X_1, axis=1)
+X_2_mean_feature = np.mean(X_2, axis=1)
 
-# print(X_0.shape[0])
-X_0_summed = np.zeros(X_0.shape[0])
-X_0_summed = np.mean(X_0, axis=1)
-X_1_summed = np.zeros(X_1.shape[0])
-X_1_summed = np.mean(X_1, axis=1)
-X_2_summed = np.zeros(X_2.shape[0])
-X_2_summed = np.mean(X_2, axis=1)
-
-# print(pd.DataFrame(X_0.flatten()).describe())
-# print(pd.DataFrame(X_1.flatten()).describe())
-# print(pd.DataFrame(X_2.flatten()).describe())
-
-# print(X_0_summed.shape)
-# print(pd.DataFrame({'features_summed': X_0_summed}))
-df_0 = pd.DataFrame({'features_summed': X_0_summed})
-df_1 = pd.DataFrame({'features_summed': X_1_summed})
-df_2 = pd.DataFrame({'features_summed': X_2_summed})
-df_new_0 = findOutliers(df_0, 'features_summed')
-df_new_1 = findOutliers(df_1, 'features_summed')
-df_new_2 = findOutliers(df_2, 'features_summed')
+perform_t_test(X_0_mean_feature[:150], X_1_mean_feature[:150])
 
 
-# plt.figure(figsize=(16,5))
-# plt.subplot(1,2,1)
-# sns.distplot(df_0)
-# plt.subplot(1,2,2)
-# sns.distplot(df_new_0)
-# plt.show()
+#print(pd.DataFrame(X_0.flatten()).describe())
+#print(pd.DataFrame(X_1.flatten()).describe())
+#print(pd.DataFrame(X_2.flatten()).describe())
 
+# from scipy.stats import f_oneway
 
 # #perform one-way ANOVA
-# F, p = f_oneway(df_0, df_1, df_2)
-# print(F)
-# print(p)
+# F, p = f_oneway(X_0, X_1, X_2)
+# # print(F)
+# # print(p)
 # # print(ALL)
 
-# t_value,p_value=stats.ttest_ind(df_0,df_1)
-t_value,p_value=stats.ttest_ind(df_new_0,df_new_1)
-print(t_value)
-print(p_value)
+km = KMeans(n_clusters=3)
 
-#sns.boxplot(X_0.flatten())
-# sns.displot(X_0_summed)
-# plt.show()
-
-
+# Creating histogram
